8c1cbe1e8d94c1819226f478d5a3b86293c83720
surajpradhank <surajkumarpradhan2016@gmail.com>
2024-07-27 16:30:11 +0530

    react using cdn

---
+++ app.js
@@ -0,0 +1,17 @@
+
+/**
+ * <div id="parent">
+ *      <div id="child1"> 
+ *          <h1> H1 tag</h1>
+ *          <h2> H2 tag</h2>
+ *      <div id="child2">
+ *          <h3> H3 tag</h>
+ *          <h4> H4 tag</h4>
+ * <div>
+ */
+
+// @ts-nocheck
+const heading = React.createElement("h1", { id: "heading" }, "Hello React");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
